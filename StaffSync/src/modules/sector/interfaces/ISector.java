504002be93fd773b01dc918c3948eb4925b31989
Evellyn Rodrigues <evellynrocha489@gmail.com>
2023-09-25 22:28:11 -0300

    feat: remove employee from createSector && add id generator employee

--- StaffSync/src/modules/sector/interfaces/ISector.java
+++ StaffSync/src/modules/sector/interfaces/ISector.java
@@ -1,9 +1,10 @@
 package modules.sector.interfaces;
 
+import modules.employee.Employee;
 import modules.sector.Sector;
 
 public interface ISector {
-    Sector createSector(String id, String name, String Employees);
+    Sector createSector(String id, String name, String description);
     void listSector();
     void deleteSector();
     Sector updateSector(String id, String name, String Employees);
